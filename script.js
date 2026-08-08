e7c26cbc7a5467cebe76434cea3601b9a7002bf2
sksairohith <sksairohith123@gmail.com>
2026-08-08 18:11:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
 function toggleTheme() {
     document.body.classList.toggle("dark");
 }
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 ||
+        document.documentElement.scrollTop > 300) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
